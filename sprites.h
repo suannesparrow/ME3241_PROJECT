643c17ffddbbbbaf096e73cf29e8d894df0ef178
suannesparrow <114264742+suannesparrow@users.noreply.github.com>
2025-04-08 17:38:01 +0800

    Create sprites.h

---
+++ sprites.h
@@ -0,0 +1,56 @@
+// sprites.h
+#ifndef SPRITES_H
+#define SPRITES_H
+
+#include "gba.h"
+
+#define NCOLS 20
+#define SNAKE_BODY 0
+#define PREY 1
+
+int palette[] = {
+    RGB(0,0,0),      // 0 Black
+    RGB(31,31,31),   // 1 White
+    RGB(0,31,31),    // 2 Light Blue
+    RGB(26,26,26),   // 3 Light Grey
+    RGB(9,9,9),      // 4 Darkest Grey
+    RGB(31,31,0),    // 5 Yellow
+    RGB(31,0,0),     // 6 Red
+    RGB(0,0,31),     // 7 Blue
+    RGB(11,8,3),     // 8 Dark Brown
+    RGB(27,27,27),   // 9 Lightest Grey
+    RGB(16,16,16),   // 10 Dark Grey
+    RGB(31,17,17),   // 11 Light Red
+    RGB(31,24,24),   // 12 Lighter Red
+    RGB(27,0,0),     // 13 Dark Red
+    RGB(0,16,0),     // 14 Darkest Green
+    RGB(0,31,0),     // 15 Pure Green
+    RGB(16,31,16),   // 16 Light Green
+    RGB(23,31,23),   // 17 Lightest Green
+    RGB(31,26,28),   // 18 Light Pink
+    RGB(31,0,15)     // 19 Bright Pink
+};
+
+u16 sprites[] = {
+// SNAKE_BODY (18 = light pink)
+18,18,18,18,18,18,18,18,
+18,18,18,18,18,18,18,18,
+18,18,18,18,18,18,18,18,
+18,18,18,18,18,18,18,18,
+18,18,18,18,18,18,18,18,
+18,18,18,18,18,18,18,18,
+18,18,18,18,18,18,18,18,
+18,18,18,18,18,18,18,18,
+
+// PREY (19 = bright pink)
+19,19,19,19,19,19,19,19,
+19,19,19,19,19,19,19,19,
+19,19,19,19,19,19,19,19,
+19,19,19,19,19,19,19,19,
+19,19,19,19,19,19,19,19,
+19,19,19,19,19,19,19,19,
+19,19,19,19,19,19,19,19,
+19,19,19,19,19,19,19,19
+};
+
+#endif
